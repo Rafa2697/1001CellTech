a45f4134088c9541ee4dddae09da37a989038093
Rafael <Rafael.mattos468@gmail.com>
2024-02-14 20:25:51 -0300

    update

---
+++ scripts.js
@@ -0,0 +1,24 @@
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('animate')
+        }else{
+            entry.target.classList.remove('animate')
+        }
+    })
+})
+const myObserver2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('animate2')
+        }else{
+            entry.target.classList.remove('animate2')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.image')
+const elementsRight = document.querySelectorAll('.image2')
+
+elements.forEach((element) => myObserver.observe(element))
+elementsRight.forEach((element) => myObserver2.observe(element))
