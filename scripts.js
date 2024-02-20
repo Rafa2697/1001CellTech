aabde3d5460a65650225edbd84ebbb459a538c7b
Rafael <Rafael.mattos468@gmail.com>
2024-02-20 17:55:50 -0300

    update

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 const myObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if(entry.isIntersecting){
-            entry.target.classList.add('animate')
+            // entry.target.classList.add('animate')
+            entry.target.classList.add('animate-div')
         }else{
-            entry.target.classList.remove('animate')
+            entry.target.classList.remove('animate-div')
         }
     })
 })
@@ -16,12 +17,23 @@ const myObserver2 = new IntersectionObserver((entries) => {
         }
     })
 })
+const myObserver3 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('animate')
+        }else{
+            entry.target.classList.remove('animate')
+        }
+    })
+})
 
-const elements = document.querySelectorAll('.image')
+const elements = document.querySelectorAll('.img-wraper')
 const elementsRight = document.querySelectorAll('.image2')
+const elementsLeft = document.querySelectorAll('.image')
 
 elements.forEach((element) => myObserver.observe(element))
 elementsRight.forEach((element) => myObserver2.observe(element))
+elementsLeft.forEach((element) => myObserver3.observe(element))
 
 
 // efeito do botão nav
